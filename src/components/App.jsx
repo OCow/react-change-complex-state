9e3c0909b404933f419e314604cfb8508f0ae62f
oliver.kuhn2@gmail.com <oliver.kuhn2@gmail.com>
2020-11-04 21:18:56 +0100

    change complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  function onChangeInput(event) {
+    //destructure event.target object 
+    const {value, name} = event.target; 
+    
+    setFullName((prevValue) => {
+      if(name === "fName") {
+        return {
+          firstName: value, 
+          lastName: prevValue.lastName 
+        };
+        } else if(name === "lName") {
+          return {
+            firstName: prevValue.firstName, 
+            lastName: value 
+          };
+        }
+      }
+    ); 
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.firstName} {fullName.lastName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={onChangeInput} name="fName" value={fullName.firstName} placeholder="First Name" />
+        <input onChange={onChangeInput} name="lName" value={fullName.lastName} placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
